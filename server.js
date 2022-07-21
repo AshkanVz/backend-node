a33ff93f383655a1e10aef2d0377c4a8b122572b
Ashkan Valizadeh <ashvalizadeh@gmail.com>
2022-07-21 14:24:24 +0430

    new change

--- server.js
+++ server.js
@@ -49,6 +49,13 @@ app.use("/api/post-category", postCategoryRouter);
 app.use("/api/post-comment", postCommentRouter);
 
 //? PORT
-const port = process.env.PORT || 5000;
+//const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+//
+//app.listen(port, () => console.log(`listening on port ${port}`));
+var http = require('http');
+http.createServer(function (req, res) {
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.end('Hello World\n');
+}).listen(process.env.PORT);
+console.log('Server running at http://127.0.0.1:1337/');
